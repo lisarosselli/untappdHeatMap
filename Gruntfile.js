149d4a9240c1e5408e310bc138fd99c580ac1aa8
Lisa Rosselli <studio1809@gmail.com>
2015-04-07 12:40:49 -0500

    adding Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		jshint: {
+			all: ['js/main.js']
+		},
+		csslint: {
+			all: ['css/*.css']
+		},
+		watch: {
+			js: {
+				files: ['js/*.js'],
+				tasks: ['jshint']
+			},
+			css: {
+				files: ['css/*.css'],
+				tasks: ['csslint']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+
+	grunt.registerTask('default', [
+		'jshint',
+		'csslint'
+	]);
+};
