127865c5dbb9d2d108c38a1b5df02eb860427f26
Lisa Rosselli <studio1809@gmail.com>
2015-05-10 12:40:52 -0500

    bit of info clean up

--- js/dataparse.js
+++ js/dataparse.js
@@ -44,10 +44,10 @@ var dataParse = {
 		h4a.innerHTML  	= data.response.beer.beer_style + '<br>' +
 										 	data.response.beer.beer_abv + '% ABV' + '<br>' +
 										 	data.response.beer.beer_ibu + ' IBU' + '<br>' +
-										 	data.response.beer.rating_score + '/5.0';		
+										 	'Rated: ' + data.response.beer.rating_score + ' / 5.0';		
 														
-		h4b.innerHTML = '<a href=\'' + data.response.beer.brewery.contact.url +'\'>' +
-											'<img style=\'height:37px;\' src=\'' + data.response.beer.brewery.brewery_label + '\'>&nbsp;' +
+		h4b.innerHTML = 	'<img style=\'height:40px;\' src=\'' + data.response.beer.brewery.brewery_label + '\'>&nbsp;' +
+											'<a href=\'' + data.response.beer.brewery.contact.url +'\'>' +
 											data.response.beer.brewery.brewery_name + '</a>';
 											
 		p.textContent = (data.response.beer.beer_description != "") ? 
@@ -57,51 +57,3 @@ var dataParse = {
 		return div;
 	}
 }
-
-/*
-	3 check-ins here
-	pull those checkins / have them in an object ready
-*/
-
-/*
-		https://developers.google.com/maps/documentation/javascript/examples/layer-heatmap
-
-	 	local pub response
-		var checkins = app.model.pubsResponse.response.checkins; (25)
-		checkins.items[n].
-					beer
-					brewery
-					checkin_comment
-					created_at
-					user
-					venue
-		
-		app.model.pubsResponse.response.checkins.items[n].venue = {}
-		app.model.pubsResponse.response.checkins.items[0].venue.location.lat
-		app.model.pubsResponse.response.checkins.items[0].venue.location.lng
-		app.model.pubsResponse.response.checkins.items[0].venue.location.venue_address
-		app.model.pubsResponse.response.checkins.items[0].venue.location.venue_city
-		app.model.pubsResponse.response.checkins.items[0].venue.venue_name
-	
-		app.model.pubsResponse.response.checkins.items[0].beer = {}
-		app.model.pubsResponse.response.checkins.items[0].beer.beer_name
-		app.model.pubsResponse.response.checkins.items[0].beer.beer_abv
-		app.model.pubsResponse.response.checkins.items[0].beer.beer_ibu
-		app.model.pubsResponse.response.checkins.items[0].beer.beer_style
-	
-		
-		app.model.pubsResponse.response.checkins.items[0].brewery.brewery_name
-		app.model.pubsResponse.response.checkins.items[0].brewery.brewery_label (jpeg)
-	
-		map.data.addGeoJson(results) results are JSON
-		the map.data object is a collection of Features (google.maps.Data.Feature)
-	
-		var a = new google.maps.Data.Feature( options...);
-		{
-			geometry: sdfas,
-		 	id: you set this,
-			properties: {
-				}
-		}
-	
-	*/
