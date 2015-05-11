9d7beb73a537093b769ff468824f69ce3fab48b8
Lisa Rosselli <studio1809@gmail.com>
2015-05-11 11:51:44 -0500

    updated gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,8 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		jshint: {
-			all: ['js/*.js']
+			all: ['js/controller.js', 'js/dataparse.js', 'js/googlemapview.js', 'js/main.js', 
+						'js/mainuiview.js', 'js/model.js', 'js/untappd.js', 'js/user.js', 'js/view.js']
 		},
 		csslint: {
 			all: ['css/*.css']
@@ -25,21 +26,37 @@ module.exports = function(grunt) {
 						sourceMap: true
 					},
 		      files: {
-		        'js/output.min.js': ['js/controller.js', 'js/dataparse.js', 'js/googlemapview.js', 'js/view.js',
-												'js/main.js', 'js/mainuiview.js', 'js/model.js', 'js/untappd.js', 'js/user.js']
+		        'js/output.min.js': ['js/controller.js', 'js/dataparse.js', 'js/googlemapview.js', 'js/main.js', 
+																'js/mainuiview.js', 'js/model.js', 'js/untappd.js', 'js/user.js', 'js/view.js']
 		      }
 		    }
-		  }
+		  },
+			cssmin: {
+			  options: {
+			    shorthandCompacting: false,
+			    roundingPrecision: -1
+			  },
+			  target: {
+			    files: {
+			      'css/output.css': ['css/styles.css']
+			    }
+			  }
+			}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-csslint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+	grunt.registerTask('test', ['jshint']);
+	grunt.registerTask('build', ['uglify', 'cssmin']);
 
 	grunt.registerTask('default', [
 		'jshint',
 		'csslint',
-		'uglify'
+		'uglify',
+		'cssmin'
 	]);
 };
